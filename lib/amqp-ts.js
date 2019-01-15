abreits/amqp-ts

commit 4aea6eba83eb61c4d229560a2fac1965082f5fd3

Employ reconnect behavior on amqplib close events (#29)
Fixes a bug where amqp-ts would fail to reconnect in the event that a
node in a RabbitMQ cluster was cycled. In this case, amqplib emits a
close event which amqp-ts fails to listen for. Fixes
https://github.com/abreits/amqp-ts/issues/27

lib/amqp-ts.js
@@ -35,6 +35,7 @@ var Connection = (function () {
         if (reconnectStrategy === void 0) { reconnectStrategy = { retries: 0, interval: 1500 }; }
         this.connectedBefore = false;
         this._rebuilding = false;
+        this._isClosing = false;
         this.url = url;
         this.socketOptions = socketOptions;
         this.reconnectStrategy = reconnectStrategy;
@@ -51,6 +52,7 @@ var Connection = (function () {
         }
         this._retry = -1;
         this._rebuilding = true;
+        this._isClosing = false;
         // rebuild the connection
         this.initialized = new Promise(function (resolve, reject) {
             _this.tryToConnect(_this, 0, function (err) {
@@ -107,7 +109,14 @@ var Connection = (function () {
                     //connection.removeListener("end", restart); // not sure this is needed
                     thisConnection._rebuildAll(err); //try to rebuild the topology when the connection  unexpectedly closes
                 };
+                var onClose = function () {
+                    connection.removeListener("close", onClose);
+                    if (!_this._isClosing) {
+                        restart(new Error("Connection closed by remote host"));
+                    }
+                };
                 connection.on("error", restart);
+                connection.on("close", onClose);
                 //connection.on("end", restart); // not sure this is needed
                 thisConnection._connection = connection;
                 callback(null);
@@ -153,6 +162,7 @@ var Connection = (function () {
     };
     Connection.prototype.close = function () {
         var _this = this;
+        this._isClosing = true;
         return new Promise(function (resolve, reject) {
             _this.initialized.then(function () {
                 _this._connection.close(function (err) {

src/amqp-ts.ts
@@ -44,6 +44,7 @@ export class Connection {
   _connection: AmqpLib.Connection;
   _retry: number;
   _rebuilding: boolean = false;
+  _isClosing: boolean = false;
 
   _exchanges: { [id: string]: Exchange };
   _queues: { [id: string]: Queue };
@@ -69,6 +70,7 @@ export class Connection {
     }
     this._retry = -1;
     this._rebuilding = true;
+    this._isClosing = false;
 
     // rebuild the connection
     this.initialized = new Promise<void>((resolve, reject) => {
@@ -130,7 +132,14 @@ export class Connection {
           //connection.removeListener("end", restart); // not sure this is needed
           thisConnection._rebuildAll(err); //try to rebuild the topology when the connection  unexpectedly closes
         };
+        var onClose = () => {
+          connection.removeListener("close", onClose);
+          if (!this._isClosing) {
+            restart(new Error("Connection closed by remote host"));
+          }
+        };
         connection.on("error", restart);
+        connection.on("close", onClose);
         //connection.on("end", restart); // not sure this is needed
         thisConnection._connection = connection;
 
@@ -181,6 +190,7 @@ export class Connection {
   }
 
   close(): Promise<void> {
+    this._isClosing = true;
     return new Promise<void>((resolve, reject) => {
       this.initialized.then(() => {
         this._connection.close(err => {

src/amqp-ts.spec-i.ts
@@ -39,7 +39,12 @@ function restartAmqpServer() {
       throw (new Error("Unable to restart rabbitmq, error:\n" + err.message));
     }
   } else {
-    throw (new Error("AmqpServer shutdown and restart not implemented for this platform"));
+    try {
+      cp.execSync("./tools/restart-rabbit.sh");
+    } catch (err) {
+      winston.log("error", "Unable to shutdown and restart RabbitMQ");
+      throw (new Error("Unable to restart rabbitmq, error:\n" + err.message));
+    }
   }
 }
 

tools/restart-rabbit.sh
@@ -1,0 +1,5 @@
+#!/bin/bash
+set -e
+container_id=`docker ps -q --filter ancestor=rabbit`
+docker exec $container_id rabbitmqctl stop_app
+docker exec $container_id rabbitmqctl start_app

commit e2eeebdbd9d7355c5b7a0a7cd1a6f20877bbe2a7

Prevent double restart on connection recovery
Fixes a minor connection recovery bug introduced in #29 in which restart
woud be invoked twice. The close event handler will no longer trigger a
rebuild if a prior error has already toggled _rebuilding to true.

lib/amqp-ts.js
@@ -111,7 +111,7 @@ var Connection = (function () {
                 };
                 var onClose = function () {
                     connection.removeListener("close", onClose);
-                    if (!_this._isClosing) {
+                    if (!thisConnection._rebuilding && !thisConnection._isClosing) {
                         restart(new Error("Connection closed by remote host"));
                     }
                 };

src/amqp-ts.ts
@@ -134,7 +134,7 @@ export class Connection {
         };
         var onClose = () => {
           connection.removeListener("close", onClose);
-          if (!this._isClosing) {
+          if (!thisConnection._rebuilding && !thisConnection._isClosing) {
             restart(new Error("Connection closed by remote host"));
           }
         };